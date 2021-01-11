b7e7ae86aeedc38d2e0d111a9e90e86cd018e390
Ola Ersson <ola.ersson@gmail.com>
2021-01-11 09:36:21 +0100

    ok

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,7 +7,7 @@ export default function Header(props: any) {
     return(
         <header className="bg-primary white">
             <div className='header-wrapper mw-1440 auto row between center-v relative'>
-                <div id='company-logo' className='fw-700 ml-15'>FÖRERETAGET.se</div>
+                <div id='company-logo' className='fw-700 ml-15'>fÖRERETAGET.se</div>
                     <div id='icon-wrapper' className='icon-wrapper mr-15 relative'>
                         <FontAwesomeIcon id='cart-icon' icon='shopping-basket' />
                     <div id='cart-amount'>0</div>
